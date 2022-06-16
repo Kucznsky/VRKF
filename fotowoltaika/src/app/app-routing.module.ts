813f432ed53c2aa98d64de9cd6e293c52a18c2cf
Kucznsky <michal.kuczynski2000@wp.pl>
2022-06-16 11:36:42 +0200

    routing

--- fotowoltaika/src/app/app-routing.module.ts
+++ fotowoltaika/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { RoofDetailsComponent } from './roof-details/roof-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: 'details', component: RoofDetailsComponent },
+
+  { path: '**', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
